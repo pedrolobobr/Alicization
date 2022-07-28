99e62c562e1f709e223d4f4c476c94bbd9aed260
Pedro Henrique Silva Santos <pgames658@gmail.com>
2022-07-28 13:54:09 -0300

    0.0.5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// select element with id = "id1"
+window.onload = function(){ 
+    document.getElementById("d20").onclick = function() {myFunction()};
+
+    function myFunction() {
+         
+        console.log(Math.random() * 10 + 1);
+        
+    } 
+};
