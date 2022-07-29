b31a73a982ee08d89ab6cdc89be868f74b2481af
PedroLobo <pgames658@gmail.com>
2022-07-28 22:24:57 -0300

    0.0.6

--- index.js
+++ index.js
@@ -1,11 +1,83 @@
-window.onload = function(){ 
+window.addEventListener('load', function(){ 
+    document.getElementById("d03").onclick = function() {myFunction()};
+
+    function myFunction() {
+         
+        var d03 = (Math.random() * 3 + 1);
+
+        var d03convertido = parseInt(d03);
+
+        alert(d03convertido)
+        
+    } 
+});
+
+window.addEventListener('load', function(){ 
+    document.getElementById("d06").onclick = function() {myFunction()};
+
+    function myFunction() {
+         
+        var d06 = (Math.random() * 6 + 1);
+
+        var d06convertido = parseInt(d06);
+
+        alert(d06convertido)
+        
+    } 
+});
+
+window.addEventListener('load', function(){ 
+    document.getElementById("d09").onclick = function() {myFunction()};
+
+    function myFunction() {
+         
+        var d09 = (Math.random() * 9 + 1);
+
+        var d09convertido = parseInt(d09);
+
+        alert(d09convertido)
+        
+    } 
+});
+
+window.addEventListener('load', function(){ 
+    document.getElementById("d10").onclick = function() {myFunction()};
+
+    function myFunction() {
+         
+        var d10 = (Math.random() * 10 + 1);
+
+        var d10convertido = parseInt(d10);
+
+        alert(d10convertido)
+        
+    } 
+});
+
+window.addEventListener('load', function(){ 
+    document.getElementById("d12").onclick = function() {myFunction()};
+
+    function myFunction() {
+         
+        var d12 = (Math.random() * 12 + 1);
+
+        var d12convertido = parseInt(d12);
+
+        alert(d12convertido)
+        
+    } 
+});
+
+window.addEventListener('load', function(){ 
     document.getElementById("d20").onclick = function() {myFunction()};
 
     function myFunction() {
          
         var d20 = (Math.random() * 20 + 1);
+
         var d20convertido = parseInt(d20);
+
         alert(d20convertido)
         
     } 
-};
+});
