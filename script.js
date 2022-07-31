e6f1364f33ccb073b05165d3412d5a359330409d
PedroLobo <pgames658@gmail.com>
2022-07-30 23:00:40 -0300

    0.0.7

--- script.js
+++ script.js
@@ -4,11 +4,8 @@ window.addEventListener('load', function(){
     function myFunction() {
          
         var d03 = (Math.random() * 3 + 1);
-
-        var d03convertido = parseInt(d03);
-
-        alert(d03convertido)
-        
+        var d03convertido = parseInt(d03)
+        document.getElementById('resultadoDados').value=d03convertido; 
     } 
 });
 
@@ -18,11 +15,8 @@ window.addEventListener('load', function(){
     function myFunction() {
          
         var d06 = (Math.random() * 6 + 1);
-
         var d06convertido = parseInt(d06);
-
-        alert(d06convertido)
-        
+        document.getElementById('resultadoDados').value=d06convertido; 
     } 
 });
 
@@ -32,11 +26,8 @@ window.addEventListener('load', function(){
     function myFunction() {
          
         var d09 = (Math.random() * 9 + 1);
-
         var d09convertido = parseInt(d09);
-
-        alert(d09convertido)
-        
+        document.getElementById('resultadoDados').value=d09convertido; 
     } 
 });
 
@@ -46,11 +37,8 @@ window.addEventListener('load', function(){
     function myFunction() {
          
         var d10 = (Math.random() * 10 + 1);
-
         var d10convertido = parseInt(d10);
-
-        alert(d10convertido)
-        
+        document.getElementById('resultadoDados').value=d10convertido; 
     } 
 });
 
@@ -60,11 +48,8 @@ window.addEventListener('load', function(){
     function myFunction() {
          
         var d12 = (Math.random() * 12 + 1);
-
         var d12convertido = parseInt(d12);
-
-        alert(d12convertido)
-        
+        document.getElementById('resultadoDados').value=d12convertido; 
     } 
 });
 
@@ -74,10 +59,9 @@ window.addEventListener('load', function(){
     function myFunction() {
          
         var d20 = (Math.random() * 20 + 1);
-
         var d20convertido = parseInt(d20);
-
-        alert(d20convertido)
-        
+        document.getElementById('resultadoDados').value=d20convertido; 
     } 
 });
+
+
